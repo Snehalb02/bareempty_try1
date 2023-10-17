faf703d0eef69fff32c0154ec16e3fbad60b257b
user <@example.com>
2023-10-17 09:25:02 +0000

    Init commit

---
+++ Tryme1/src/main/java/com/example/Tryme1/Tryme1Application.java
@@ -0,0 +1,13 @@
+package com.example.Tryme1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tryme1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tryme1Application.class, args);
+	}
+
+}
---
+++ Tryme1/src/test/java/com/example/Tryme1/Tryme1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Tryme1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tryme1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
